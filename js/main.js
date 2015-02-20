0dfbc5f51dfce99292564fa39aab54d06a742b44
davidnewreld <davidnewreld@users.noreply.github.com>
2015-02-20 17:40:56 +0100

    man sieht was

--- js/main.js
+++ js/main.js
@@ -1,51 +1,23 @@
 var map = L.map('map').setView([53.5510846, 9.99368179999999], 13);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  maxZoom: 18
 }).addTo(map);
 
-
-var test = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "stroke": "#555555",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#555555",
-        "fill-opacity": 0.5
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              10.00255823135376,
-              53.55559352906138
-            ],
-            [
-              10.007193088531494,
-              53.55319706845856
-            ],
-            [
-              9.998974800109863,
-              53.55118291681195
-            ],
-            [
-              9.995133876800537,
-              53.55457377518183
-            ],
-            [
-              10.00255823135376,
-              53.55559352906138
-            ]
-          ]
-        ]
+L.TopoJSON = L.GeoJSON.extend({
+  addData: function (jsonData) {
+    if (jsonData.type === "Topology") {
+      for (key in jsonData.objects) {
+        geojson = topojson.feature(jsonData, jsonData.objects[key]);
+        L.GeoJSON.prototype.addData.call(this, geojson);
       }
+    } else {
+      L.GeoJSON.prototype.addData.call(this, jsonData);
     }
-  ]
-}
+  }
+});
+// Copyright (c) 2013 Ryan Clark
 
-L.geoJson(hastadtteile).addTo(map);
+var topoLayer = new L.TopoJSON();
+topoLayer.addData(wahllok);
+topoLayer.addTo(map);
